67d0913c74d6679b129e48add2f9d2977869eb23
kabaka <iankabaka1@gmail.com>
2025-01-16 19:02:38 +0300

    python getting remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,20 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        """
+        Fetches the response body from the URL as bytes.
+        """
+        response = requests.get(self.url)
+        response.raise_for_status()  # Raise an error for HTTP status codes 4xx/5xx
+        return response.content  # Returns the content in bytes
 
     def load_json(self):
-        pass
+        """
+        Parses the JSON response body into a Python list of dictionaries.
+        """
+        response_body = self.get_response_body()
+        return json.loads(response_body)  # Converts JSON bytes to a Python object
